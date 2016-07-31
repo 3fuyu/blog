b0aac76ac4858413ac7c559074428f387361b8b3
3fuyu <sdlgdxzhangyu@163.com>
2016-07-31 21:09:49 +0800

    some config file

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by 3fuyu on 16/7/31.
+ */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
