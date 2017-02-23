d3ac0f95dfbc6f2d446aa13a1d8631087a601d61
3fuyu <banxianyu@gmail.com>
2017-02-23 21:01:52 +0800

    upload button

--- web/src/app/js/components/management/Media.js
+++ web/src/app/js/components/management/Media.js
@@ -2,12 +2,40 @@
  * Created by 3fuyu on 2017/2/14.
  */
 
-import React, {PropTypes, Component} from 'react';
+import React, {PropTypes, Component} from "react";
+import RaisedButton from "../../../../../node_modules/material-ui/RaisedButton";
+
+const styles = {
+    button: {
+        margin: 12,
+    },
+    exampleImageInput: {
+        cursor: 'pointer',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        width: '100%',
+        opacity: 0,
+    },
+};
 
 class Media extends Component {
+
+
     render() {
         return (
-            <div>test</div>
+            <div>
+                <RaisedButton
+                    label="Choose an Image"
+                    labelPosition="before"
+                    style={styles.button}
+                    containerElement="label"
+                >
+                    <input type="file" style={styles.exampleImageInput}/>
+                </RaisedButton>
+            </div>
         );
     }
 }
