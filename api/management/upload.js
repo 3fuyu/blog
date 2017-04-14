f66ddc770f8e5c1920749f1dd4e2d9fa9b3ee4f6
3fuyu <banxianyu@gmail.com>
2017-04-14 21:10:24 +0800

    修改上传接口

--- api/management/upload.js
+++ api/management/upload.js
@@ -18,18 +18,18 @@ let uploadApis = [{
         let cacheFolder = 'public/images/uploadcache/';
         let currentUser = req.session.user;
         let userDirPath = cacheFolder + currentUser.id;
+        let form = new formidable.IncomingForm(); //创建上传表单
+        let displayUrl;
 
         if (!fs.existsSync(userDirPath)) {
             fs.mkdirSync(userDirPath);
         }
 
-        let form = new formidable.IncomingForm(); //创建上传表单
         form.encoding = 'utf-8'; //设置编辑
         form.uploadDir = userDirPath; //设置上传目录
         form.keepExtensions = true; //保留后缀
         form.maxFieldsSize = 2 * 1024 * 1024; //文件大小
         form.type = true;
-        let displayUrl;
 
         form.parse(req, function (err, fields, files) {
             if (err) {
