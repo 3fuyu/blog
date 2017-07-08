9722377e8b5a7e52728bc870c2261c2b1393b8cf
3fuyu <banxianyu@gmail.com>
2017-07-08 11:28:35 +0800

    about logo

--- web/src/app/js/components/index/About.js
+++ web/src/app/js/components/index/About.js
@@ -7,33 +7,28 @@ import Power from './About_power';
 import '../../../css/about.less'
 
 class About extends Component {
-    render () {
+    render() {
         return (
             <div id="about">
                 <div className="head">
                     <img className="logo" src="images/3fuyu_bg.jpg" alt=""/>
                     <p className="discription">3Fuyu</p>
                 </div>
-                <div className="line"></div>
                 <div className="body">
-                    <div className="left">
-                        <div className="life">
-                            <p className="title">Life</p>
-                            <p>姓名： 张宇</p>
-                            <p>年龄： {new Date().getFullYear() - 1993}</p>
-                            <p>爱好： 跑步（不是一般能跑）</p>
-                            <p>偶像： Steve Jobs</p>
-                        </div>
-                        <div className="work">
-                            <p className="title">Work</p>
-                            <p>职业： 前端攻城狮（只是会<br/>
-                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;一点点PS，别误会）</p>
-                            <p>Email: sdlgdxzhangyu@163.com</p>
-                        </div>
+
+                    <div className="life">
+                        <p className="title">About Me</p>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            3fuyu(张宇)，93年生，平常喜欢跑步，大学跑了三年，工作之后有时间就会跑，现在基本每晚四公里。另外还是Steve
+                            Jobs的狂热粉丝，自传读过不下3遍，因此也被“扭曲厂力”感染上了轻微强迫症。</p>
+                        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            其实，我的真实身份是一枚前端攻城狮，狂热的技术爱好者。如今正是前端遍地开花的时代，一个月不吸收点新知识发发，回头再看前端就是一脸懵逼的状态。因此也提醒自己达摩克利斯之剑一直悬在头上。平常喜欢折腾技术，比如这个blog（该站建成于2017.6，基于React + Node + Mongodb，其中之前的技术文章来源于本人2014年至今用wordpress记录的文章）就是用腻了wordpress，hexo等等套路，想搞一些不仅仅限于技术文章，完全自己控制的个人站，比如Laboratory，会持续放出自己实验性的项目。</p>
                     </div>
-                    <div className="right">
-                        <p className="title">POWER</p>
-                        <Power/>
+                    <div className="work">
+                        <p className="title">Work</p>
+                        <p>职业： 前端攻城狮（只是会<br/>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;一点点PS，别误会）</p>
+                        <p>Email: sdlgdxzhangyu@163.com</p>
                     </div>
                 </div>
             </div>
