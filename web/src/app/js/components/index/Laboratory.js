b76c1ec24fba20378631e495aa760f06bacceab2
3fuyu <banxianyu@gmail.com>
2017-07-28 20:57:18 +0800

    完善 laboratory 入口

--- web/src/app/js/components/index/Laboratory.js
+++ web/src/app/js/components/index/Laboratory.js
@@ -6,17 +6,41 @@ import {Component} from 'react';
 import '../../../css/laboratory.less';
 
 class About extends Component {
+    state = {
+        laboratories: [{
+            title: '胜率很高的五子棋',
+            pic: './images/gomoku.jpg',
+            discription: '纯js，利用五子棋核心算法，每一步棋计算当前和下一步共196种权重可能',
+            url: 'http://3fuyu.com/laboratory/gomoku/index.html'
+        }]
+    }
+
     componentWillMount () {
         document.body.scrollTop = 0;
     }
+    goto (url) {
+        window.open(url);
+    }
     render() {
+        let t = this;
+
         return (
             <div id="laboratory">
-                <ul>
-                    <li>
-                        <div>五子棋</div>
-                        <div>不会输的五子棋</div>
-                    </li>
+                <div className="top">
+                    <h2 className="title">Laboratory</h2>
+                    <p className="discription">less but butter</p>
+                </div>
+                <ul className="list">
+                    {this.state.laboratories.map(function (value, key) {
+                        return (
+                            <li className="list-item" key={key} onClick = {() => t.goto(value.url)}>
+                                <div className="photo"><img src={value.pic} alt=""/></div>
+                                <div className="title">{value.title}</div>
+                                <div className="discription">{value.discription}</div>
+                                <i className="light"></i>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         );
